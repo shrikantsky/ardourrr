78fb70c07228453a98178349dde26ab11c7a0bb8
GaaCBot <noreply@gaac.vercel.app>
2024-05-13 17:57:56 +0530

    Uploaded ServicePagePart.js via GAAC

---
+++ uploads/ServicePagePart.js
@@ -0,0 +1,262 @@
+
+import React, { useEffect } from 'react';
+import './ServicePage.css';
+import '../Component/ServicePart2.css'
+
+
+
+
+
+
+// https://raw.githubusercontent.com/shrikantsky/ardourrr/main/uploads/ServicePart2.css
+
+
+
+function ServicePagePart() {
+
+  return (
+  
+    <div>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
+      {/* 
+        shrikantall css */}
+      
+      <link rel="shortcut icon" type="image/png" href="https://fagerstrom.studio/wp-content/uploads/2022/09/Favicon.png" sizes="32x32" />
+      <div id="content-holder" data-active-post={72}>
+        <div id="content-72" className="content-container active-content  ">
+          <div className="transition-wrap">
+            <div className="sections">
+              {/* this is for upper one conatiner 3 boxes */}
+              <section id="section_ab2860cdb" className="content-block" data-column-mode-sm="single" data-column-mode-xs="single">
+                <div className="container">
+                  <div id="row_42ab43c58" className="row">
+                    <div id="column_5d0b6e97c" className="column" data-xl-width={4}>
+                      <div className="content-wrapper">
+                        <div id="content_1e30b0bee" className="column-content" data-module="image">
+                          <div className="ce-image" data-align="left"><a className="image-link" href="https://fagerstrom.studio/work/salad" target="_self"><img className="is-content" src="https://fagerstrom.studio/wp-content/uploads/2023/01/Salad_01.jpg" width={2179} height={3212} alt="Salad_01" caption data-width="original" data-scaling="no" /></a></div>
+                        </div>
+                        <div id="content_bac6e5b94" className="column-content" data-module="text">
+                          <div className="is-content">
+                            <p style={{ fontSize: '0.778rem', lineHeight: '0.833rem' }} data-mce-style="font-size: 0.778rem; line-height: 0.833rem;" data-font-size-xl="0.778rem" data-line-height-xl="0.833rem" data-font-size-md="0.611rem" data-line-height-md="0.667rem" data-font-size-sm="0.778rem" data-line-height-sm="0.833rem" data-font-size-xs="0.778rem" data-line-height-xs="0.833rem" data-font-size-lg="0.667rem" data-line-height-lg="0.722rem"><span className="font_o42vza0vs">Salad</span><br /><span face="pitch-sans-semibold, sans-serif" data-mce-style="font-family: pitch-sans-semibold, sans-serif;" style={{ fontFamily: 'pitch-sans-semibold, sans-serif' }}>Healthy
+                              apparel</span></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div id="column_bc8586c81" className="column" data-xl-width={4}>
+                      <div className="content-wrapper">
+                        <div id="content_c92fcd4d1" className="column-content" data-module="image">
+                          <div className="ce-image" data-align="left"><a className="image-link" href="https://fagerstrom.studio/work/aegles" target="_self"><img className="is-content" src="https://fagerstrom.studio/wp-content/uploads/2022/07/fagerstrom-home02-scaled.jpg" width={1737} height={2560} alt="fagerstrom-home-aegles" caption="Aegle's | Healing Sustainable Skincare" data-width="original" data-scaling="no" /></a></div>
+                        </div>
+                        <div id="content_78959ba4f" className="column-content" data-module="text">
+                          <div className="is-content">
+                            <p style={{ fontSize: '0.778rem', lineHeight: '0.833rem' }} data-mce-style="font-size: 0.778rem; line-height: 0.833rem;" data-font-size-xl="0.778rem" data-line-height-xl="0.833rem" data-font-size-md="0.611rem" data-line-height-md="0.667rem" data-font-size-lg="0.667rem" data-line-height-lg="0.722rem"><span data-mce-style="font-family: pitch-sans-semibold, sans-serif;" style={{ fontFamily: 'pitch-sans-semibold, sans-serif' }}>Aegle's<br />Glowing
+                              skin naturally</span></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div id="column_9803280e1" className="column" data-xl-width={4}>
+                      <div className="content-wrapper">
+                        <div id="content_9dc5ee654" className="column-content" data-module="image">
+                          <div className="ce-image" data-align="left"><a className="image-link" href="https://fagerstrom.studio/work/chaser" target="_self"><img className="is-content" src="https://fagerstrom.studio/wp-content/uploads/2022/07/fagerstrom-home03-scaled.jpg" width={1737} height={2560} alt="fagerstrom-home-chaser" caption="Chaser | Ergonomic Cybersecurity" data-width="original" data-scaling="no" /></a></div>
+                        </div>
+                        <div id="content_8e9e47351" className="column-content" data-module="text">
+                          <div className="is-content">
+                            <p style={{ fontSize: '0.778rem', lineHeight: '0.833rem' }} data-mce-style="font-size: 0.778rem; line-height: 0.833rem;" data-font-size-xl="0.778rem" data-line-height-xl="0.833rem" data-font-size-md="0.611rem" data-line-height-md="0.667rem" data-font-size-lg="0.667rem" data-line-height-lg="0.722rem"><span className="font_o42vza0vs">Chaser</span><br /><span className="font_o42vza0vs">Ergonomic cybersecurity</span></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              {/* this div is ended here */}
+              {/* thi is sfor spacer-container */}
+              <section id="section_3a383fc8d" className="content-block" data-column-mode-sm="single" data-column-mode-xs="single">
+                <div className="container">
+                  <div id="row_40eceb894" className="row">
+                    <div id="column_dd7f51050" className="column" data-xl-width={12}>
+                      <div className="content-wrapper">
+                        <div id="content_921c7ffc9" className="column-content" data-module="spacer">
+                          <div className="spacer-container">
+                            <div className="is-content">
+                              <div className="spacer">{/* horizontal spacer */}</div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              {/* this div is for  an independendt design studio */}
+              <section id="section_93e66042f" className="content-block" data-column-mode-sm="single" data-column-mode-xs="single">
+                <div className="container">
+                  <div id="row_e7a52b562" className="row">
+                    <div id="column_3bd868a0a" className="column spacer-column" data-xl-width={4}>
+                      <div className="content-wrapper">
+                      </div>
+                    </div>
+                    <div id="column_165db2ee5" className="column" data-xl-width={8}>
+                      <div className="content-wrapper">
+                        <div id="content_77d282f26" className="column-content" data-module="text">
+                          <div className="is-content">
+                            <p style={{ fontSize: '2.5rem', lineHeight: '2.944rem' }} data-mce-style="font-size: 2.5rem; line-height: 2.944rem;" data-font-size-xl="2.500rem" data-line-height-xl="2.944rem" data-font-size-lg="1.556rem" data-line-height-lg="2.000rem" data-font-size-md="1.444rem" data-line-height-md="1.889rem" data-font-size-sm="1.444rem" data-line-height-sm="1.889rem" data-font-size-xs="1.444rem" data-line-height-xs="1.889rem"><span className="font_k0j2wyy58">fagerström is an independent design
+                              studio developing unique, cohesive &amp; meaningful solutions
+                              for bold brands that stray from the ordinary.</span></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="row_2ff3ef435" className="row">
+                    <div id="column_7bd4ffe4a" className="column" data-xl-width={12}>
+                      <div className="content-wrapper">
+                        <div id="content_200bbc23d" className="column-content" data-module="spacer">
+                          <div className="spacer-container">
+                            <div className="is-content">
+                              <div className="spacer">{/* horizontal spacer */}</div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              {/* independent design studio end here */}
+
+              {/* this is div belongs to the whole conatiner  */}
+              <section id="section_a030892b5" className="content-block" data-column-mode-sm="single" data-column-mode-xs="single">
+                <div className="container">
+                  {/* this  div is for 4 big boxes */}
+                  <div id="row_a74b0ab60" className="row">
+                    <div id="column_a90ab0b2f" className="column" data-xl-width={12}>
+                      <div className="content-wrapper">
+                        {/* this  div is for 4 big boxes */}
+                        <div id="content_1f8db28d0" className="column-content" data-module="portfoliogrid">
+                          <div id="masonry-content_1f8db28d0" className="masonry" data-masonry-init="normal">
+                            <div className="masonry-item-width" />
+                            <div id="project-1230" className="masonry-item thumb masonry-content_1f8db28d0-item below-left cat-5 " data-xl-width={6} data-sm-width={6} data-xs-width={12}><a href="https://fagerstrom.studio/work/decassa" className="pg-link">
+                              <div className="thumb-inner">
+                                <div className="thumb-hover">
+                                  <div className="thumb-hover-meta top-left hide-both fade">
+                                    <p>
+                                      <span className="title" data-font="regular">Decassa</span><br />
+                                      <span className="category" data-font="regular">Live
+                                        your inspiration</span>
+                                    </p>
+                                  </div>
+                                </div>
+                                <img src="https://fagerstrom.studio/wp-content/uploads/2024/04/DECASSA_Thumbnail.jpg" width={2629} height={1746} alt="DECASSA_Lifestyle_Branding" />
+                              </div>
+                            </a><a className="pg-title-link pg-link" href="https://fagerstrom.studio/work/decassa" title="Decassa">
+                                <div className="post-title font_o42vza0vs">Decassa<span className="font_o42vza0vs">Live your inspiration</span>
+                                </div>
+                              </a></div>
+                            <div id="project-927" className="masonry-item thumb masonry-content_1f8db28d0-item below-left cat-5  video-hover" data-xl-width={6} data-sm-width={6} data-xs-width={12}><a href="https://fagerstrom.studio/work/eiuchre" className="pg-link">
+                              <div className="thumb-inner">
+                                <div className="thumb-hover">
+                                  <div className="background-video">
+                                    <video webkit-playsinline playsInline loop muted data-object-fit="cover">
+                                      <source src="https://fagerstrom.studio/wp-content/uploads/2023/03/EIUCHRE®_06.mp4" type="video/mp4" />
+                                    </video>
+                                  </div>
+                                  <div className="thumb-hover-meta top-left hide-both fade">
+                                    <p>
+                                      <span className="title" data-font="regular">Eiuchre</span><br />
+                                      <span className="category" data-font="regular">Time
+                                        to feel</span>
+                                    </p>
+                                  </div>
+                                </div>
+                                <img src="https://fagerstrom.studio/wp-content/uploads/2023/06/eiuchre.jpg" width={2455} height={1632} alt="Eiuchre_Fagerström" />
+                              </div>
+                            </a><a className="pg-title-link pg-link" href="https://fagerstrom.studio/work/eiuchre" title="Eiuchre">
+                                <div className="post-title font_o42vza0vs">Eiuchre<span className="font_o42vza0vs">Time to feel</span></div>
+                              </a></div>
+                          </div>
+                        </div>
+                        {/* this  div is for 4 big boxes ended */}
+                      </div>
+                    </div>
+                  </div>
+                  {/* this  div is for 4 big boxes ended  whole 4*/}
+                  {/* this div is for small box  container */}
+                  {/* this is sthe end of the small box  */}
+                </div>
+              </section>
+              {/* this is end of  the div belongs to the whole conatiner  */}
+
+
+              <div className="content-wrapper">
+              </div>
+            </div>
+            <div id="column_3e257a4fe" className="column" data-xl-width={2} data-md-width={4} data-lg-width={3}>
+              <div className="content-wrapper">
+              </div>
+            </div>
+            <div id="column_097f0284b" className="column spacer-column" data-xl-width={1} data-md-width={1} data-lg-width={1}>
+              <div className="content-wrapper">
+              </div>
+            </div>
+            <div id="column_86b885778" className="column" data-xl-width={1} data-md-width={2} data-lg-width={2}>
+              <div className="content-wrapper">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pswp" tabIndex={-1} role="dialog" aria-hidden="true">
+        <div className="pswp__bg" />
+        <div className="pswp__scroll-wrap">
+          <div className="pswp__container">
+            <div className="pswp__item" />
+            <div className="pswp__item" />
+            <div className="pswp__item" />
+          </div>
+          <div className="pswp__ui pswp__ui--hidden">
+            <div className="pswp__top-bar">
+              <div className="pswp__counter" />
+              <button className="pswp__button pswp__button--close" title="Close (Esc)" />
+              <button className="pswp__button pswp__button--share" title="Share" />
+              <button className="pswp__button pswp__button--fs" title="Toggle fullscreen" />
+              <button className="pswp__button pswp__button--zoom" title="Zoom in/out" />
+              <div className="pswp__preloader">
+                <div className="pswp__preloader__icn">
+                  <div className="pswp__preloader__cut">
+                    <div className="pswp__preloader__donut" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+              <div className="pswp__share-tooltip" />
+            </div>
+            <button className="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
+            </button>
+            <button className="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
+            </button>
+            <div className="pswp__caption">
+              <div className="pswp__caption__center" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="back-to-top" data-arrow-align="right">
+        <a className="semplice-event" data-event-type="helper" data-event="scrollToTop"><svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="53px" height="20px" viewBox="0 0 53 20" enableBackground="new 0 0 53 20" xmlSpace="preserve">
+          <g id="Ebene_3">
+          </g>
+          <g>
+            <polygon points="43.886,16.221 42.697,17.687 26.5,4.731 10.303,17.688 9.114,16.221 26.5,2.312 	" />
+          </g>
+        </svg>
+        </a>
+      </div>
+    </div>
+  );
+}
+export default ServicePagePart
